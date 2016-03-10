19f57bb154a102983fbbbb2c43c1f6eac9e31708
Christian <mrchristianw@gmail.com>
2016-03-10 17:33:11 -0500

    added code from class

---
+++ christian/Blinkled/blink.c
@@ -0,0 +1,23 @@
+//*****************************************************************************
+//
+// MSP432 main.c template - P1.0 port toggle
+//
+//****************************************************************************
+
+#include "msp.h"
+
+void main(void)
+{
+    volatile uint32_t i;
+
+    WDTCTL = WDTPW | WDTHOLD;           // Stop watchdog timer
+
+    // The following code toggles P1.0 port
+    P1DIR |= BIT0;                      // Configure P1.0 as output
+
+    while(1)
+    {
+        P1OUT ^= BIT0;                  // Toggle P1.0
+        for(i=10000; i>0; i--);         // Delay
+    }
+}
