9d2656ea29dcb10ec3fffef34f76f5a75f42f24b
kyalth <kyalth@gmail.com>
2016-03-16 16:40:03 -0400

    Add to accept time debounce and which switch to use

--- Kyle/myHeaders/debounce.h
+++ Kyle/myHeaders/debounce.h
@@ -9,18 +9,18 @@
 #include "D:\Common\School\Files\EGR1440\myHeaders\mySystick.h"
 #endif
 
-#define SW1IN (*((volatile uint8_t *)(0x42098004)))
-int myDebounce()
+//#define SW1IN (*((volatile uint8_t *)(0x42098004)))
+int myDebounce(uint8_t button, int ms)
 {
   static int oldState = 0;
-  int State = SW1IN;
+  int State = button;
   if(State == 0)
     {
-      SysTick_Init(50);
+      SysTick_Init(ms);
       int bounce = 0;
       while (bounce == 0 && (/*SYSTICK_STCSR*/(HWREG32(0xE000E010)) & 0x00010000)==0)
 	{
-	  State = SW1IN;
+	  State = button;
 	  if (State != oldState)
 	    {
 	      bounce = 1;
