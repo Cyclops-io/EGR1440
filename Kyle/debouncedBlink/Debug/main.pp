6a2f1cac01feb991eb4b9bdb8ee51cd8a648b3bf
kyalth <kyalth@gmail.com>
2016-03-16 15:08:27 -0400

    Update current build
    rebuild main using clean headers
    resign main.c

--- Kyle/debouncedBlink/Debug/main.pp
+++ Kyle/debouncedBlink/Debug/main.pp
@@ -18,6 +18,7 @@ main.obj: D:/Windows10/Apps/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/std
 main.obj: D:/Windows10/Apps/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/linkage.h
 main.obj: D:/Windows10/Apps/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/_lock.h
 main.obj: D:/Common/School/Files/EGR1440/myHeaders/mySetup.h
+main.obj: D:/Common/School/Files/EGR1440/myHeaders/SRS.h
 main.obj: D:/Common/School/Files/EGR1440/myHeaders/debounce.h
 main.obj: D:/Common/School/Files/EGR1440/myHeaders/mySystick.h
 
@@ -39,5 +40,6 @@ D:/Windows10/Apps/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/stdarg.h:
 D:/Windows10/Apps/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/linkage.h: 
 D:/Windows10/Apps/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.7/include/_lock.h: 
 D:/Common/School/Files/EGR1440/myHeaders/mySetup.h: 
+D:/Common/School/Files/EGR1440/myHeaders/SRS.h: 
 D:/Common/School/Files/EGR1440/myHeaders/debounce.h: 
 D:/Common/School/Files/EGR1440/myHeaders/mySystick.h: 
--- Kyle/debouncedBlink/main.c
+++ Kyle/debouncedBlink/main.c
@@ -4,6 +4,8 @@
 //
 //****************************************************************************
 
+#define SETUP  11
+
 #include "msp.h"
 #include "stdbool.h"
 //no commit for path changes
@@ -13,18 +15,28 @@
 
 void main(void)
 {
-	int setupState = 01;
-    setup(setupState);
+	const int setupState = SETUP;
 
-    P2DIR |= BIT0|BIT1|BIT2;	//port 2 direction
-  	    //current contence or with BIT0(P2.0) or BIT1(P2.1) or BIT2(P2.2)
-  		//BITs can be found in msp.h -> msp432p401r.h
-    P1DIR |= 0b00000001;	//port 1 direction all original state but P1.0 is output
-    P1REN = 0b00010010;		//port 1 reg enable
-    P1OUT = 0b00010010;		//port 1 our put ||||input pullup resistor
-    enum STATE{RS = 0, S = 1};
+    //P1 config data to be OR'ed with the current state
+    //of each reg
+    uint8_t p1dir = 0b00000001;
+    uint8_t p1ren = 0b00010010;
+    uint8_t p1out = 0b00010010;
+
+    //P2 config data to be OR'ed with the current state
+    //of each reg
+    uint8_t p2dir = BIT0|BIT1|BIT2;
+    uint8_t p2ren = 0b00000000;
+	uint8_t p2out = 0b00000000;
+
+    setup(setupState);
+    setP2(p2dir,p2ren,p2out);
+    setP1(p1dir,p1ren,p1out);
+    setP3P4P5P6P7P8P9P20();
+
     int previousState = 0;
     enum STATE state= RS;
+
     while(true)
       {
     	int currentState = myDebounce();
