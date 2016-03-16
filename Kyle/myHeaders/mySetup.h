2d79d9f50599052dd0958a3e35aef66850b88b89
kyalth <kyalth@gmail.com>
2016-03-16 15:05:28 -0400

    Expand and clean
    clean up enum global issue
    clean switch case
    add p1 and p2 setup
    function to disable all other ports

--- Kyle/myHeaders/mySetup.h
+++ Kyle/myHeaders/mySetup.h
@@ -5,26 +5,72 @@
 #include "msp.h"
 #endif
 
-void setup(int opts)
+//enum STATE{RS = 0, S = 1};
+#if SETUP == 10 || SETUP == 11
+#include "D:\Common\School\Files\EGR1440\myHeaders\SRS.h"
+#endif
+
+void setup(const int opts)
 {
-	//int x,y;
 	switch (opts)
 	{
 	case 01: // Stop watchdog timer
 		WDTCTL = WDTPW | WDTHOLD;
 		break;
 	case 10:
-		//x = 1;
 		break;
 	case 11:
 		WDTCTL = WDTPW | WDTHOLD;
-		//y = 1;
+		break;
+	default:
 		break;
 	}
-	//if((x ==1)|(y==1)) enum STATE{RS = 0, S = 1};
-
 }
 
+void setP1(uint8_t dir, uint8_t ren, uint8_t out)
+{
+    P1DIR |= dir;
+    P1REN |= ren;
+    P1OUT |= out;
+}
+void setP2(uint8_t dir, uint8_t ren, uint8_t out)
+{
+    P2DIR |= dir;
+    P2REN |= ren;
+    P2OUT |= out;
+}
+setP3P4P5P6P7P8P9P20()
+{
+	P3DIR |= 0b00000000;
+	P3REN |= 0b00000000;
+	P3OUT |= 0b00000000;
+
+	P4DIR |= 0b00000000;
+	P4REN |= 0b00000000;
+	P4OUT |= 0b00000000;
+
+	P5DIR |= 0b00000000;
+	P5REN |= 0b00000000;
+	P5OUT |= 0b00000000;
+
+	P6DIR |= 0b00000000;
+	P6REN |= 0b00000000;
+	P6OUT |= 0b00000000;
+
+	P7DIR |= 0b00000000;
+	P7REN |= 0b00000000;
+	P7OUT |= 0b00000000;
+
+	P8DIR |= 0b00000000;
+	P8REN |= 0b00000000;
+	P8OUT |= 0b00000000;
+
+	P9DIR |= 0b00000000;
+	P9REN |= 0b00000000;
+	P9OUT |= 0b00000000;
+
+	P10DIR |= 0b00000000;
+	P10REN |= 0b00000000;
+	P10OUT |= 0b00000000;
+}
 #endif
-
-
